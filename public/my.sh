770bef98c9b0b65efa3596d3ad7d41145903df91
Antal Áron <antalaron@antalaron.hu>
2024-07-23 09:44:53 +0200

    Use antalaron.hu

--- public/my.sh
+++ public/my.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 cp ~/.bashrc ~/.bashrc.bak
-curl -o ~/.bashrc https://antalaron.codes/config/bashrc
+curl -o ~/.bashrc https://antalaron.hu/config/bashrc 2>/dev/null
 
 if command -v git &> /dev/null; then
     git config --global user.name "Antal Áron"
     git config --global user.email "antalaron@antalaron.hu"
 
-    curl -o ~/.gitignore https://antalaron.codes/config/gitignore
+    curl -o ~/.gitignore https://antalaron.hu/config/gitignore 2>/dev/null
     git config --global core.excludesfile ~/.gitignore
     git config --global init.defaultBranch master
 fi
