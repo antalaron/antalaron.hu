4832938a72365f76fdeeb2f5b67912b09c234818
Antal Áron <antalaron@antalaron.hu>
2024-07-23 09:36:11 +0200

    Add dotend installer my.sh

---
+++ public/my.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cp ~/.bashrc ~/.bashrc.bak
+curl -o ~/.bashrc https://antalaron.codes/config/bashrc
+
+if command -v git &> /dev/null; then
+    git config --global user.name "Antal Áron"
+    git config --global user.email "antalaron@antalaron.hu"
+
+    curl -o ~/.gitignore https://antalaron.codes/config/gitignore
+    git config --global core.excludesfile ~/.gitignore
+fi
+
+echo "Done! Please restart your terminal."
